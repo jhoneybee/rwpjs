73cf34511a44c7da9935048420d9002b44db51eb
jhoneybee <zhangjin19960908@gmail.com>
2020-05-19 15:35:01 +0800

    fix: 添加对class的支持

--- packages/render-react/src/models/config.js
+++ packages/render-react/src/models/config.js
@@ -96,6 +96,8 @@ exports.default = function (config, status) {
                     presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
                     plugins: [
                         '@babel/plugin-transform-runtime',
+                        '@babel/plugin-proposal-class-properties',
+                        '@babel/plugin-proposal-object-rest-spread',
                         styleImport
                     ]
                 },
