71ecc8a4c1087e712c2f4f93edd8e0f3fa975fd1
jhoneybee <zhangjin19960908@gmail.com>
2020-07-07 13:17:40 +0800

    fix: 修复部署文档失败

--- packages/react-ui/.umirc.ts
+++ packages/react-ui/.umirc.ts
@@ -6,6 +6,8 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  publicPath: '/rwpjs/',
+  base: '/rwpjs',
   extraBabelPlugins: [
     [
       'import',
