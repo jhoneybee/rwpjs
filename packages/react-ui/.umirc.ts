030c626c5c0d33de9d56941bfe65420ce5059ba2
jhoneybee <zhangjin19960908@gmail.com>
2020-07-14 11:45:34 +0800

    docs: 添加github的地址

--- packages/react-ui/.umirc.ts
+++ packages/react-ui/.umirc.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'RWP.JS',
-  hash: true,
   favicon: 'https://user-images.githubusercontent.com/24241052/87370901-bdf6a880-c5b6-11ea-9237-56625c403237.png',
   logo: 'https://user-images.githubusercontent.com/24241052/87370901-bdf6a880-c5b6-11ea-9237-56625c403237.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  navs: [
+    null,
+    { title: 'GitHub', path: 'https://github.com/jhoneybee/rwpjs' },
+  ],
   extraBabelPlugins: [
     [
       'import',
