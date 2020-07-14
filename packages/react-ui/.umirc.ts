c38aa02d7fa7150043c3d5eb943f399af450ffc8
jhoneybee <zhangjin19960908@gmail.com>
2020-07-14 11:01:47 +0800

    fix: 修复文档刷新404的问题

--- packages/react-ui/.umirc.ts
+++ packages/react-ui/.umirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'RWP.JS',
+  hash: true,
   favicon: 'https://user-images.githubusercontent.com/24241052/87370901-bdf6a880-c5b6-11ea-9237-56625c403237.png',
   logo: 'https://user-images.githubusercontent.com/24241052/87370901-bdf6a880-c5b6-11ea-9237-56625c403237.png',
   outputPath: 'docs-dist',
