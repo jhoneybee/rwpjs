78cf73f157fce23e20778ae93104dd7e92408cc6
jhoneybee <zhangjin19960908@gmail.com>
2020-07-22 13:53:07 +0800

    fix: 修复Space导出

--- packages/react-ui/src/space/index.tsx
+++ packages/react-ui/src/space/index.tsx
@@ -1,5 +1,1 @@
-import React from 'react';
-import { Space as AntSpace } from 'antd';
-import { SpaceProps } from 'antd/lib/space';
-
-export const Space = (props: SpaceProps) => <AntSpace {...props} />
+export { Space } from 'antd';
