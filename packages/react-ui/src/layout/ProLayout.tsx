7be10e255294866ce88e6be76f2c685c16d1af4d
jhoneybee <zhangjin19960908@gmail.com>
2020-07-27 19:16:49 +0800

    feat: 添加ProLayout的高度100%

--- packages/react-ui/src/layout/ProLayout.tsx
+++ packages/react-ui/src/layout/ProLayout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
 import AntProLayout, { BasicLayoutProps } from '@ant-design/pro-layout'
+import './style/index.less'
 
 export const ProLayout = (props: BasicLayoutProps) => <AntProLayout {...props}/>
