08fcceb17a58d3c42897c88947415353cc433398
zhangj <zhangjin19960908@gmail.com>
2020-09-23 19:58:30 +0800

    fix: 添加ci来进行发布npm库，如果不符合release的标题则不进行发布，仅仅只是更新网页 (#158)
    * fix: 添加修改
    
    * fix: 调整流水线
    
    * fix: 调整流水线
    
    * fix: 修复校验逻辑
    
    * fix: 修复校验逻辑
    
    * feat: 添加正式版本的ci执行
    
    * feat: 查看当前的release的ci
    
    * fix: 修复表格列不显示的问题
    
    * fix: 删除多余的引用
    
    * fix: 删除多余的引用
    
    * fix: 添加ci来进行发布npm库，如果不符合release的标题则不进行发布，仅仅只是更新网页
    
    * fix: 修改cd的脚本
    
    * fix: 修改cd的脚本

--- script/release-canary.js
+++ script/release-canary.js
@@ -4,24 +4,13 @@ const { writeFileSync } = require('fs');
 
 const PR_TITLE = process.env.PR_TITLE
 
-const { exec } = require("child_process");
-const reactUIPackages = require('../packages/react-ui/package.json')
-
+console.log(PR_TITLE)
 if (/release:\s+.*/.test(PR_TITLE)) {
     const version = PR_TITLE.replace(/release:\s+/i, '').trim()
     reactUIPackages.version = version
-    // 如果脚本存在于历史版本中,则不进行发布
-    exec('npm view @rwp/react-ui versions', (error, stdout) => {
-        if (error) {
-            throw error 
-        }
-        const versions = JSON.parse(stdout.replace(/'/g, '"'))
-        if (!versions.includes(reactUIPackages.version)) {
-            writeFileSync(join(__dirname, '..', 'packages', 'react-ui', 'package.json'), JSON.stringify(reactUIPackages))
-            execSync(`cd ./packages/react-ui/ && npm publish`)
-            execSync(`git commit -m '${version}'`)
-            execSync(`git push`)
-            execSync('cd packages/react-ui && npm install && npm publish --access public --tag canary')
-        }
-    })
+    writeFileSync(join(__dirname, '..', 'packages', 'react-ui', 'package.json'), JSON.stringify(reactUIPackages))
+    execSync(`cd ./packages/react-ui/ && npm publish`)
+    execSync(`git commit -m '${version}'`)
+    execSync(`git push`)
+    execSync('cd packages/react-ui && npm install && npm publish --access public --tag canary')
 }
