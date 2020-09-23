53b1a37464c183c5243fc056f4b4e44ea0ecbdb6
jhoneybee <zhangjin19960908@gmail.com>
2020-09-23 20:58:33 +0800

    fix: 修改ci

--- script/release-canary.js
+++ script/release-canary.js
@@ -2,26 +2,29 @@ const { execSync, exec } = require("child_process");
 const { join } = require('path')
 const { writeFileSync } = require('fs');
 
-const PR_TITLE = process.env.PR_TITLE
 
 const reactUIPackages = require('../packages/react-ui/package.json')
 
-console.log(PR_TITLE)
-if (/release:\s+.*/.test(PR_TITLE)) {
-    const version = PR_TITLE.replace(/release:\s+/i, '').trim()
-    reactUIPackages.version = version
-    // 如果脚本存在于历史版本中,则不进行发布
-    exec('npm view @rwp/react-ui versions', (error, stdout) => {
-        if (error) {
-            throw error 
-        }
-        const versions = JSON.parse(stdout.replace(/'/g, '"'))
-        if (!versions.includes(reactUIPackages.version)) {
-            writeFileSync(join(__dirname, '..', 'packages', 'react-ui', 'package.json'), JSON.stringify(reactUIPackages))
-            execSync(`cd ./packages/react-ui/ && npm publish`)
-            execSync(`git commit -m '${version}'`)
-            execSync(`git push`)
-            execSync('cd packages/react-ui && npm install && npm publish --access public --tag canary')
-        }
-    })
-}
+exec('git log -p -1',(error, stdout) => {
+    const commitText = stdout.toString()
+    if (/release:\s+.*/.test(commitText)) {
+        const release = /release:\s+.*/.exec(commitText)[0]
+        const version = release.replace(/release:\s+/i, '').trim()
+        reactUIPackages.version = version
+        // 如果脚本存在于历史版本中,则不进行发布
+        exec('npm view @rwp/react-ui versions', (error, stdout) => {
+            if (error) {
+                throw error 
+            }
+            const versions = JSON.parse(stdout.replace(/'/g, '"'))
+            if (!versions.includes(reactUIPackages.version)) {
+                writeFileSync(join(__dirname, '..', 'packages', 'react-ui', 'package.json'), JSON.stringify(reactUIPackages))
+                execSync(`cd ./packages/react-ui/ && npm publish`)
+                execSync(`git commit -m '${version}'`)
+                execSync(`git push`)
+                execSync('cd packages/react-ui && npm install && npm publish --access public --tag canary')
+            }
+        })
+    }
+
+})
