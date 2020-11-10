e7519828097d9e90032bca43e7b7a48f56b6aa35
jhoneybee <zhangjin19960908@gmail.com>
2020-11-10 12:49:07 +0800

    feat: 开启ssr方便搜索引擎检索

--- packages/react-ui/.umirc.ts
+++ packages/react-ui/.umirc.ts
@@ -6,6 +6,17 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/24241052/87370901-bdf6a880-c5b6-11ea-9237-56625c403237.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  metas: [{
+    name: 'keywords',
+    content: 'rwp, rwpjs, RWP, RWPJS'
+  }, {
+      name: 'description',
+      content: '☘ 企业级的React开发的解决方案'
+  }],
+  ssr: {
+  },
+  exportStatic: {
+  },
   targets: {
     ie: 11,
   },
