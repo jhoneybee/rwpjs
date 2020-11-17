d22da0c3784fc741cdb0eb30166411d3622dd9ec
zhangj <zhangjin19960908@gmail.com>
2020-11-17 13:25:56 +0800

    feat: 添加保存完成后, 会触发对应的onSaveFile事件 (#356)

--- packages/react-ui/src/incubator/index.tsx
+++ packages/react-ui/src/incubator/index.tsx
@@ -5,6 +5,8 @@ import { Modal, ModalHandle } from '../modal'
 let incubatorSocket = new WebSocket("ws://localhost:29580")
 
 export interface PreviewFileParams {
+    // 监听回调的id
+    id?: string
     // 要预览的文件名
     fileName: string
     // 文件的下载地址
@@ -30,12 +32,21 @@ export interface IncubatorProps {
      */
     downloadAddress: string
 
+    /**
+     * 当前实例，可以用来open打开
+     */
     incubator: React.MutableRefObject<IncubatorHandle | null>
+    
+    /**
+     * 保存文件触发的事件
+     */
+    onSaveFile?: (params: Object) => void
 }
 
 export const Incubator = ({
     downloadAddress,
-    incubator
+    incubator,
+    onSaveFile
 }: IncubatorProps) => {
     const modal = useRef<ModalHandle>(null)
 
@@ -62,14 +73,22 @@ export const Incubator = ({
         // 如果没有连接上则提示用户下载插件
         if (incubatorSocket.readyState === incubatorSocket.CLOSED) {
             modal.current?.show();
+            
             // 每4秒尝试重新连接
             const reconnect = setInterval(() => {
                 if (incubatorSocket.readyState === incubatorSocket.CLOSED) {
                     incubatorSocket = new WebSocket("ws://localhost:29580")
+                    incubatorSocket.onmessage = (socket) => {
+                        onSaveFile?.(socket.data)
+                    }
                 } else {
                     clearInterval(reconnect);
                 }
             }, 4000)
+        } else {
+            incubatorSocket.onmessage = (socket) => {
+                onSaveFile?.(socket.data)
+            }
         }
     }, [])
 
