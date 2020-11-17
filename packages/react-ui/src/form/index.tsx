5621c9fb19647fd656764f2da3b378f360f55c24
jhoneybee <zhangjin19960908@gmail.com>
2020-11-17 11:18:03 +0800

    fix: 优化Form的固定label宽度

--- packages/react-ui/src/form/index.tsx
+++ packages/react-ui/src/form/index.tsx
@@ -1,15 +1,9 @@
-import React, { useRef, useEffect, isValidElement } from 'react'
+import React from 'react'
 import { Form as AntForm } from 'antd'
 import { useForm } from 'antd/lib/form/Form'
-import classNames from 'classnames'
-import { isArray } from 'lodash'
 
 import { FormProps, FormItemProps } from '../interface'
-import { classPrefix } from '../utils'
 import './style/index.less'
-
-
-const classNameFixedWidth = `${classPrefix}-item-fixed-width`
 
 export const Form = (props: FormProps) => {
     const { cols = 5, form: propsForm, children, ...restProps } = props
@@ -18,58 +12,18 @@ export const Form = (props: FormProps) => {
         propsForm.current = form
     }
 
-    const formRef = useRef<HTMLDivElement | null>(null);
-
-    const changeFormItemDom = () => {
-        if (formRef.current) {
-            let isEnableLabelWidth = false;
-            if (isArray(children)) {
-                children.some(ele => {
-                    if (isValidElement(ele) && ele.props.labelWidth) {
-                        isEnableLabelWidth = true
-                        return true
-                    }
-                    return false
-                })
-            } else if (isValidElement(children)) {
-                if (children.props.labelWidth) {
-                    isEnableLabelWidth = true
-                }
-            }
-
-            if (isEnableLabelWidth) {
-                const antFormItems = formRef.current.querySelectorAll('.ant-form-item')
-                antFormItems.forEach(items => {
-                    if (items.getAttribute('style')?.includes('--label-width')) {
-                        const newClassName = items.getAttribute('class')?.replace('ant-row', '').replace(classNameFixedWidth, '')!
-                        items.setAttribute('class', classNames({
-                            [newClassName]: true,
-                            [classNameFixedWidth]: true
-                        }))
-                    }
-                })
-            }
-        }
-    }
-    
-    useEffect(() => {
-        changeFormItemDom()
-    })
-
     return (
-        <div ref={formRef}>
-            <AntForm
-                {...restProps}
-                style={{
-                    display: 'grid',
-                    gridTemplateColumns: `repeat(${cols}, auto)`,
-                    columnGap: 4,
-                }}
-                form={form}
-            >
-                {children}
-            </AntForm>
-        </div>
+        <AntForm
+            {...restProps}
+            style={{
+                display: 'grid',
+                gridTemplateColumns: `repeat(${cols}, auto)`,
+                columnGap: 4,
+            }}
+            form={form}
+        >
+            {children}
+        </AntForm>
     )
 }
 
